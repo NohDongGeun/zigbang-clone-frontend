2b735496f4ba362fc16f311f7c5e87fd1c929ade
NohDongGeun <sam573526@gmail.com>
2021-03-15 18:21:24 +0900

    Add Molecules/FilterFixed

---
+++ src/components/molecules/Filter/FilterFixed/index.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta } from "@storybook/react";
+import React from "react";
+import FilterFixed from "./index";
+import {
+  radios,
+  text,
+  withKnobs,
+} from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+
+export default {
+  title: "Molecules/FilterFixed",
+  component: FilterFixed,
+  decorators: [withKnobs],
+} as Meta;
+
+enum DealType {
+  all = "전체",
+  deposit = "전세",
+  rent = "월세",
+}
+
+export const filterFixed: React.FC = () => {
+  return (
+    <article className={"flex w-full sm:w-400"}>
+      <FilterFixed
+        label={text("label", "검색 조건을 설정해주세요.")}
+        value={radios("value", DealType, DealType.all)}
+        handleDealType={action("handleDealType")}
+        handleFilter={action("handleFilter")}
+      />
+    </article>
+  );
+};
---
+++ src/components/molecules/Filter/FilterFixed/index.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { Button, Img, Text } from "../../..";
+import arrowUp from "../../../../assets/img/arrowUp.png";
+import filter_button from "../../../../assets/img/filter_button.png";
+
+interface IFilterHeader {
+  /**
+   * filter dealtype value
+   */
+  value: "전체" | "전세" | "월세";
+
+  /**
+   * filter된 옵션 label
+   */
+  label: string;
+
+  /**
+   * dealtype onClick
+   */
+  handleDealType: () => void;
+
+  /**
+   * filter 창 열기
+   */
+  handleFilter: () => void;
+}
+
+const FilterFixed: React.FC<IFilterHeader> = ({
+  value,
+  label,
+  handleDealType,
+  handleFilter,
+}) => {
+  return (
+    <section
+      className={
+        "flex flex-row p-1 w-full justify-center items-center border border-gray-500"
+      }
+    >
+      <div className={"flex w-1/6"}>
+        <Button
+          onClick={handleDealType}
+          className={
+            "flex w-full flex-row flex-grow-0 flex-shrink-1 h-8 border border-gray-300 justify-center items-center text-gray-700"
+          }
+        >
+          <Text label={value} className={"sm:text-sm text-xs"} />
+          <Img
+            src={arrowUp}
+            className={"h-4 w-4  sm:h-5 sm:w-5"}
+            alt={"거래유형 선택"}
+          />
+        </Button>
+      </div>
+      <div className={"flex w-5/6 pl-1 "}>
+        <Button
+          onClick={handleFilter}
+          className={"flex w-full flex-row  h-8 justify-center items-center "}
+        >
+          <div
+            className={
+              "flex flex-grow-0 w-9/12 flex-shrink-1 border border-gray-300 border-r-0 h-full justify-center items-center px-3"
+            }
+          >
+            <Text
+              label={label}
+              className={
+                "sm:text-sm text-xs truncate whitespace-nowrap text-gray-700"
+              }
+            />
+          </div>
+          <div
+            className={
+              "flex flex-row flex-grow-0 w-3/12 flex-shrink-1 border border-gray-900 h-full justify-center items-center"
+            }
+          >
+            <Img
+              src={filter_button}
+              className={"sm:w-5 sm:h-5 w-4 h-4"}
+              alt={"필터 열기"}
+            />
+            <Text
+              label={"필터"}
+              className={"sm:text-sm text-xs text-gray-900"}
+            />
+          </div>
+        </Button>
+      </div>
+    </section>
+  );
+};
+
+export default FilterFixed;
