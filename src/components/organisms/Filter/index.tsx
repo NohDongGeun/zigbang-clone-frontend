f1284a24d0712471d5e503b1e9ecc237b49fbe9d
NohDongGeun <sam573526@gmail.com>
2021-03-24 11:14:16 +0900

    Map 추가

--- src/components/organisms/Filter/index.tsx
+++ src/components/organisms/Filter/index.tsx
@@ -98,7 +98,7 @@ const Filter: React.FC = () => {
       <article
         className={`w-full sm:w-400 ${
           able && "h-400"
-        }  sm:w-400 sm:absolute sm:transform sm:translate-y-5 sm:translate-x-5 bg-gray-200 border border-gray-300 shadow-md`}
+        }  sm:w-400 sm:absolute sm:transform sm:translate-y-5 sm:translate-x-5 bg-gray-200 border border-gray-300 shadow-md z-10`}
       >
         <FilterFixed
           label={
